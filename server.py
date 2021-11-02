4d4e89bbffd0880daff9cec537bacf8dff2b61e2
neverdieone <you@example.com>
2021-11-02 09:27:33 +0300

    Add 2 buses on map

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+import json
+
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
 
@@ -5,8 +7,14 @@ async def echo_server(request):
     ws = await request.accept()
     while True:
         try:
-            message = await ws.get_message()
-            await ws.send_message(message)
+            message = {
+                "msgType": "Buses",
+                "buses": [
+                    {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
+                    {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+                ]
+            }
+            await ws.send_message(json.dumps(message))
         except ConnectionClosed:
             break
 
