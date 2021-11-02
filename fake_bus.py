7b769d421d843a8f67f71f7c292fecfa16e5e3b4
neverdieone <you@example.com>
2021-11-02 10:38:06 +0300

    Add many buses on routes

--- fake_bus.py
+++ fake_bus.py
@@ -1,9 +1,14 @@
 import json
 import os
-from itertools import cycle
+from itertools import cycle, islice
+from random import randint
 
 import trio
 from trio_websocket import open_websocket_url
+
+
+def generate_bus_id(route_id, bus_index):
+    return f'{route_id}-{bus_index}'
 
 
 def load_routes(directory_path='routes'):
@@ -14,31 +19,34 @@ def load_routes(directory_path='routes'):
                 yield json.load(file)
 
 
-async def run_bus(url, bus_id, route):
+async def run_bus(url, route_id, route, bus_index):
     async with open_websocket_url(url) as ws:
-        for coordinate in cycle(route):
+        start_position = randint(1, 30)
+        for coordinate in cycle(islice(route, start_position, len(route))):
             lat, lng = coordinate
             message = {
-                'busId': bus_id,
+                'busId': generate_bus_id(route_id, bus_index),
                 'lat': lat,
                 'lng': lng,
-                'route': bus_id
+                'route': route_id
             }
 
             await ws.send_message(json.dumps(message, ensure_ascii=False))
-            await trio.sleep(0.1)
+            await trio.sleep(1)
 
 
 async def main():
     try:
         async with trio.open_nursery() as nursery:
             for route in load_routes():
-                nursery.start_soon(
-                    run_bus,
-                    'ws://localhost:8080',
-                    route['name'],
-                    route['coordinates']
-                )
+                for bus_index in range(2):
+                    nursery.start_soon(
+                        run_bus,
+                        'ws://localhost:8080',
+                        route['name'],
+                        route['coordinates'],
+                        bus_index
+                    )
     except Exception as e:
         print(e)
 
