ab1139a40de6c1cacbde6a71c1a87c608d57ffe0
neverdieone <you@example.com>
2021-11-02 09:38:13 +0300

    Read coordinates from file for one route

--- server.py
+++ server.py
@@ -3,23 +3,33 @@ import json
 import trio
 from trio_websocket import serve_websocket, ConnectionClosed
 
-async def echo_server(request):
+
+def read_route_file(route_number):
+    with open(f'routes/{route_number}.json', 'r') as f:
+        return json.load(f)
+
+
+async def server(request):
     ws = await request.accept()
-    while True:
+    route = read_route_file(156)
+    coordinates = route['coordinates']
+
+    for coordinate in coordinates:
+        lat, lng = coordinate
         try:
             message = {
                 "msgType": "Buses",
                 "buses": [
-                    {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
-                    {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+                    {"busId": "c790сс", "lat": lat, "lng": lng, "route": "156"},
                 ]
             }
             await ws.send_message(json.dumps(message))
+            await trio.sleep(1)
         except ConnectionClosed:
             break
 
 async def main():
-    await serve_websocket(echo_server, '127.0.0.1', 8000, ssl_context=None)
+    await serve_websocket(server, '127.0.0.1', 8000, ssl_context=None)
 
 
 if __name__ == '__main__':   
